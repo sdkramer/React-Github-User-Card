010c063c46617e376582f6db4cef6dca2d42fe52
Sean Kramer <seandominickramer@gmail.com>
2020-06-05 21:28:07 -0400

    Add UserCard component with functionality

--- github-user-card-reactapp/src/App.js
+++ github-user-card-reactapp/src/App.js
@@ -1,13 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
+import UserCard from './Components/UserCard';
+import Followers from './Components/Followers';
 
-function App() {
+class App extends React.Component {
+constructor(props) {
+  super(props);
+  this.state = {
+    user: {},
+    followers: []
+  }
+  
+}
+
+componentDidMount () {
+  axios
+  .get("https://api.github.com/users/sdkramer")
+  .then (res => {
+    // console.log(res.data);
+    this.setState({ user: res.data})
+  })
+
+  axios
+  .get('https://api.github.com/users/sdkramer/followers')
+  .then (res => {
+    console.log(res.data);
+    this.setState({
+      followers: res.data
+    })
+  })
+}
+
+render() {
   return (
     <div className="App">
 <h1>Sean Kramer on Github</h1>
+<UserCard user={this.state.user}/>
+<Followers user={this.state.user}/>
     </div>
   );
+}
+
+
 }
 
 export default App;
---
+++ github-user-card-reactapp/src/Components/Followers.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+class Followers extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log(props);
+    
+  }
+
+  render () {
+    return (
+      <div className='followers-container'>
+      <img src={this.props.user.avatar_url} alt='my photo'/>
+      <div>
+      <h3>{this.props.user.name}</h3>
+  <p>{this.props.user.bio}</p>
+  <p>Location: {this.props.user.location}</p>
+  <p>Github: <a href={this.props.user.html_url}>link to github</a></p>
+  <p></p>
+      </div>
+  
+
+    </div>
+    )
+  }
+}
+
+export default Followers;
