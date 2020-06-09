dfcafdbc96b376b74ba80f733d5454a7f47b0308
Sean Kramer <seandominickramer@gmail.com>
2020-06-09 17:48:19 -0400

    Add map function to display followers data

--- github-user-card-reactapp/src/Components/Followers.js
+++ github-user-card-reactapp/src/Components/Followers.js
@@ -1,32 +1,32 @@
 import React from 'react';
 
 const Followers = (props) => {
-  // constructor(props) {
-  //   super(props);
-   
-    
-  // }
+
   
- 
+console.log(props.followers);
+
+const followersInfo = props.followers.map((follower) => {
+  return <img src={follower.avatar_url} />
+})
+
+return <div>{followersInfo}</div>
+
+
+  //   return (
     
 
-const fs  = props.followers;
-console.log(fs[2]);
-    return (
-    
+  //     <div>Followers</div>
 
-      <div>Followers</div>
-
-  //       {fs.map(follower => (
-  //         <div className='followers-container'>
-  // <img src={follower.avatar_url} alt='follower photo'/>
-  // <h3>{follower.login}</h3>
-  // <p>Github: <a href={follower.html_url}>link to github</a></p>
-  // <div>
-  //       ))}
+  // //       {fs.map(follower => (
+  // //         <div className='followers-container'>
+  // // <img src={follower.avatar_url} alt='follower photo'/>
+  // // <h3>{follower.login}</h3>
+  // // <p>Github: <a href={follower.html_url}>link to github</a></p>
+  // // <div>
+  // //       ))}
     
     
-    );
+  //   );
   }
 
 
--- github-user-card-reactapp/src/Components/UserCard.js
+++ github-user-card-reactapp/src/Components/UserCard.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 class UserCard extends React.Component {
- constructor(props) {
-   super(props)
-  //  console.log(props);
+//  constructor(props) {
+//    super(props)
+//   //  console.log(props);
    
- }
+//  }
 
   render() {
     return (
